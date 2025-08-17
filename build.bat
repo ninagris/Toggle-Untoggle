05d3fe53b01a6c9f3220bf19d16779239216721a
yafta98 <yafta98@gmail.com>
2025-08-16 20:50:52 -0400

    Updated build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 REM Activate your conda environment
-call conda activate toggle-untoggle   REM <-- replace myenv with your environment name
+call conda activate toggle-untoggle   REM
 
 REM Run PyInstaller
-pyinstaller main.py --onedir --windowed --icon=icons/icon.png ^
---add-data "icons/icon.png;icons" ^
+pyinstaller main.py --onedir --windowed --icon=icons/icon.ico ^
+--add-data "icons/icon.ico;icons" ^
 --add-data "icons/pen.png;icons" ^
 --add-data "icons/eraser.png;icons" ^
 --name=Toggle-Untoggle ^
